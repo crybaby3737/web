f8b09ac570605f8a4f979050f5a881211891a72a
crybaby3737 <arminkamitz13@hotmail.de>
2026-01-28 13:08:41 +0100

    some Settings changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
-function getMilk(){
-    Math.round(5);
-}
+// var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+// function whoPaying(names){
+// var numberOfPeople = names.length;
+// var randompersonPosition = Math.floor(Math.random() * numberOfPeople);
+// var randomPerson = names[randompersonPosition];
+
+// return randomPerson + "is going to buy lunch today!";}
+
+// console.log("He ist paying "+whoPaying(names)+ " with key "+randompersonPosition);
+
+
+// function beer(){
+//    var counter = 99;
+//    while (counter>0){
+//     console.log(counter+" bottle of beer on the wall, "+counter+" bottles of beer.");
+//     counter--;
+//     console.log("Take 1 down, pass it arround," +counter+ "bottles of beer on the wall.") ;
+//     if (counter ===0){
+//         console.log("No bottles left...")
+//     }
+//    }
+
+// }
+// beer();
